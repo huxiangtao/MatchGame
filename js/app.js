00d3837e60a9396688e83ae754b2a441dd2a1bf4
huxiangtao <elliot130@163.com>
2013-08-22 17:29:02 +0800

    add

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: huxiangtao
+ * Date: 13-8-22
+ * Time: обнГ5:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function() {
+    'use strict';
+
+    var app = {
+
+        init: function() {
+
+        }
+
+    };
+
+    window.app = app;
+
+})();
