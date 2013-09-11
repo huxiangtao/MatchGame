2da8dc1c2ff918ae33cb90fe03f43dbf9810e399
huxiangtao <elliot130@163.com>
2013-09-11 16:46:11 +0800

    add

--- js/model/palyingCard.js
+++ js/model/palyingCard.js
@@ -8,6 +8,8 @@
 
 (function(card) {
     'use strict'
+    var _validSuits,
+        _rankStrings = ["?","A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
     function PlayingCard() {
         this.superClass = card.Card;
@@ -17,6 +19,46 @@
 
     PlayingCard.prototype = Object.create(card.Card.prototype);
 
+    var playingCardMethods = {
+
+        contents: function() {
+            return _rankStrings[this.rank()] + this.suit();
+        },
+
+        suit: function(v) {
+            if(v === undefined) {
+                return this._suit ? this._suit : "?";
+            } else {
+                if (PlayingCard.validSuits[v]) {
+                    this._suit = v;
+                }
+            }
+            return this._suit;
+        },
+
+        rank: function(v) {
+            if(v === undefined) {
+                return this._rank;
+            } else {
+                if(v <= PalyingCard.maxRank()) {
+                    this._rank = v;
+                }
+                return this._rank;
+            }
+        }
+    };
+
+    Object.keys(playingCardMethods).forEach(function(name) {
+        this[name] = playingCardMethods[name];
+    },PlayingCard.prototype);
+
+    PlayingCard.validSuits = function() {
+        if(!_validSuits) {
+            _validSuits = {"?" : 1,"?" : 1,"?" : 1,"?" : 1};
+        }
+        return _validSuits;
+    }
+
     function exports() {
         return new PlayingCard();
     }
@@ -24,4 +66,5 @@
     exports.PlayingCard = PlayingCard;
 
     window.playingCard = exports;
+    debugger;
 })(window.card);
